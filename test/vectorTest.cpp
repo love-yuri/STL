846288be767256c4897f65a39a6f7470fbc0e5c8
love-yuri <yuri2078170658@gmail.com>
2024-12-30 16:42:45 +0800

    fix(test/vectorTest.cpp): 修改vector测试并增加输出信息

--- test/vectorTest.cpp
+++ test/vectorTest.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-12-30 15:27:02
- * @LastEditTime: 2024-12-30 15:43:59
+ * @LastEditTime: 2024-12-30 16:39:21
  * @Description: vector 测试
  */
 #include "../yuri_log.hpp"
@@ -13,8 +13,14 @@ int main() {
 		v.push_back(i);
 	}
 
+	v[8] = 888; // 修改测试
+	// v.at(99); // 越界测试
+	yinfo << v;
 	yinfo << "size: " << v.size();
 	yinfo << "capacity: " << v.capacity();
-	yinfo << v;
+	yinfo << "empty: "  << v.empty();
+	yinfo << "front: " << v.front();
+	yinfo << "back: " << v.back();
+	
 	return 0;
 }
